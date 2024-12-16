81ddc80f19a1464869101f27c556e1f475f40a54
Prakash <ningureprakash@gmail.com>
2024-12-16 21:25:07 +0530

    normal

--- src/commponent/pages/Contact.jsx
+++ src/commponent/pages/Contact.jsx
@@ -6,9 +6,9 @@ const Contact = () => {
         <>
             <div class="container">
                 <div class="row overflow-hidden">
-                    <div class="col-12 col-md-10 offset-md-1 mt-5">
+                    <div class="col-12 col-md-8 offset-md-2 mt-5">
                         <div class="row text-center">
-                            <div class="col-12 col-sm-6 col-md-4 mb-5 wow fadeInDown" data-wow-delay=".5s">
+                            <div class="col-12 col-sm-6 col-md-4 mb-2 wow fadeInDown" data-wow-delay=".5s">
                                 {/* <span class="fa fa-map-marker fa-3x text-primary"></span> */}
                                 <svg xmlns="http://www.w3.org/2000/svg" width="46" height="46" fill="currentColor" class="bi bi-pin-map" viewBox="0 0 16 16">
                                     <path fill-rule="evenodd" d="M3.1 11.2a.5.5 0 0 1 .4-.2H6a.5.5 0 0 1 0 1H3.75L1.5 15h13l-2.25-3H10a.5.5 0 0 1 0-1h2.5a.5.5 0 0 1 .4.2l3 4a.5.5 0 0 1-.4.8H.5a.5.5 0 0 1-.4-.8z" />
@@ -17,7 +17,7 @@ const Contact = () => {
                                 <h2 class="h5 my-3 text-dark">Address Information</h2>
                                 <p class="text-muted">Level 13, 2 Elizabeth St, Melbourne, Victor 2000</p>
                             </div>
-                            <div class="col-12 col-sm-6 col-md-4 mb-5 wow fadeInDown" data-wow-delay=".8s">
+                            <div class="col-12 col-sm-6 col-md-4 mb-2 wow fadeInDown" data-wow-delay=".8s">
                                 {/* <span class="fa fa-phone fa-3x text-primary"></span> */}
                                 <svg xmlns="http://www.w3.org/2000/svg" width="46" height="46" fill="currentColor" class="bi bi-telephone" viewBox="0 0 16 16">
                                     <path d="M3.654 1.328a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 1.169-.45 1.77a17.6 17.6 0 0 0 4.168 6.608 17.6 17.6 0 0 0 6.608 4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 0-.063-1.015l-2.307-1.794a.68.68 0 0 0-.58-.122l-2.19.547a1.75 1.75 0 0 1-1.657-.459L5.482 8.062a1.75 1.75 0 0 1-.46-1.657l.548-2.19a.68.68 0 0 0-.122-.58zM1.884.511a1.745 1.745 0 0 1 2.612.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.68.68 0 0 0 .178.643l2.457 2.457a.68.68 0 0 0 .644.178l2.189-.547a1.75 1.75 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.6 18.6 0 0 1-7.01-4.42 18.6 18.6 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877z" />
@@ -26,17 +26,13 @@ const Contact = () => {
                                 <p class="text-muted">adminsupport@website.com</p>
                                 <p class="text-muted">+333 111 111 000</p>
                             </div>
-                            <div class="col-12 col-sm-6 col-md-4 mb-5 wow fadeInDown" data-wow-delay="1.1s">
+                            <div class="col-12 col-sm-6 col-md-4 mb-2 wow fadeInDown" data-wow-delay="1.1s">
                                 {/* <span class="fa fa-share fa-3x text-primary"></span> */}
                                 <svg xmlns="http://www.w3.org/2000/svg" width="46" height="46" fill="currentColor" class="bi bi-share" viewBox="0 0 16 16">
                                     <path d="M13.5 1a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3M11 2.5a2.5 2.5 0 1 1 .603 1.628l-6.718 3.12a2.5 2.5 0 0 1 0 1.504l6.718 3.12a2.5 2.5 0 1 1-.488.876l-6.718-3.12a2.5 2.5 0 1 1 0-3.256l6.718-3.12A2.5 2.5 0 0 1 11 2.5m-8.5 4a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3m11 5.5a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3" />
                                 </svg>
                                 <h2 class="h5 my-3 text-dark">Stay In Touch</h2>
-                                <ul class="list-inline">
-                                    {/* <li class="list-inline-item"><a href="#" target="_blank"><i class="fab fa-facebook-f text-muted"></i></a></li>
-                                    <li class="list-inline-item"><a href="#" target="_blank"><i class="fab fa-instagram text-muted"></i></a></li>
-                                    <li class="list-inline-item"><a href="#" target="_blank"><i class="fab fa-twitter text-muted"></i></a></li>
-                                    <li class="list-inline-item"><a href="#" target="_blank"><i class="fab fa-linkedin text-muted"></i></a></li> */}
+                                <ul class="list-inline">                                   
                                     <li class="list-inline-item">
                                         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-facebook" viewBox="0 0 16 16">
                                             <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951" />
@@ -60,17 +56,17 @@ const Contact = () => {
                                 </ul>
                             </div>
                         </div>
-                        <div class="text-center my-5">
+                        <div class="text-center mb-5">
                             <h1 class=" fw-bold text-dark">Contact Us</h1>
                             <p class="mt-3 text-muted">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal.</p>
                         </div>
                     </div>
                 </div>
 
-                <div class="row mb-5 overflow-hidden">
+                <div class="row mb-5  overflow-hidden">
                     <div class="col-12 col-sm-6 ">
                         <form>
-                            <div class="form-group">
+                            <div class="form-group mt-2">
                                 <input type="text" class="form-control p-3 mb-2" placeholder="Name" />
                             </div>
                             <div class="form-group">
